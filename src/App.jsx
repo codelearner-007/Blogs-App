d9ee15a929048d1ba5f3c8a7379bb58ad212ecab
codelearner-007 <farhanahmad0819@gmail.com>
2025-04-28 16:19:03 +0500

    blog app start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div className="">
+      <BrowserRouter></BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
